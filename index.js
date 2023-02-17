c25472424ab0c093a2ca367d037185f874c9c601
jobeuse <iradukundajob3@gmail.com>
2023-02-17 23:29:52 +0200

    feat(json-server cors) : installing cors and adding it on routes

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('db.json') 
+var cors = require('cors') 
  
+server.use(cors())
 server.use(router)
 server.listen(3000, () => {
   console.log('JSON Server is running')
