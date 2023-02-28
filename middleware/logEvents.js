f1b97fe2803048da069f29dee2b7c20aa63b8611
jobeuse <iradukundajob3@gmail.com>
2023-02-28 23:14:49 +0200

    ft(node & express -cors): setting up cors to whiteList the ip addresses that are allowed to access these apis

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,5 +1,20 @@
+const fs= require('fs');
+const uud= require('uuid');
+
 const logger = (req,res,next) => {
 
+    if (!fs.existsSync('./logs')){
+
+        fs.mkdirSync('./logs', (error) => {
+            console.log("Creating folder \t"+error);
+        });
+    }
+    const logs=`${new Date}\t ${req.method} \t ${req.headers.origin} \t ${req.url} \n`
+    fs.appendFile('./logs/log.txt', logs, (error)=>{
+        console.log("Files Writing \t"+error);
+    });
 
     next();
 }
+
+module.exports = {logger: logger}
--- server.js
+++ server.js
@@ -2,19 +2,40 @@ const express = require('express');
 const app= express();
 const path= require('path');
 const port=process.env.PORT || 3000;
+const cors=require('cors');
 const {logger } = require('./middleware/logEvents');
 
+// third party middleware 
+const whiteList= ['http://127.0.0.1:5500','http://localhost:3000'];
+const corsOptions = {
+    origin:function(origin,callback){
+        if(whiteList.indexOf(origin) !== -1){
+            callback(null, true)
+        }else{
+            callback(new Error("You are not allowed to access this apis"));
+        }
+    },
+    optionsSuccessStatus:200
+}
+app.use(cors(corsOptions));
 // custom middleware
-// app.use(logger);
+app.use(logger);
 
 // built in middleware to handle urlencoded
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+
 // this is for rendering the static files
 app.use(express.static(path.join(__dirname,'/public')));
 
 app.get('/', (req, res) => {
       res.send('hello world');
 });
+
+app.get('/posts', (req, res) => {
+      res.send('hello world');
+});
+
 
 app.post('/posts', (req, res) => {
       console.log(req.body);
