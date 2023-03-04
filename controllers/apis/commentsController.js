81eb1777358f9840e68eb06c9bc4089395d2e443
jobeuse <iradukundajob3@gmail.com>
2023-03-04 17:06:54 +0200

    ft(nodeJs-express-comments): ft to (update,delete) comments ,viewing comments for each posts and proper response in json formt

--- controllers/apis/commentsController.js
+++ controllers/apis/commentsController.js
@@ -3,27 +3,63 @@ const Comment=require('../../models/comments');
 
 const comment_index = (req,res) =>{
  Comment.find().then((result) =>{
-        console.log(JSON.stringify(result));
-        res.send(res);
-    }).catch((err) =>{
-        console.log(err);
+         return res
+                .send({ message: "viewing comment",data:result});
+    }).catch((err) => {
+        return  res.send({ message: "Error while viewing comment",error: err.message })
     });
 }
 
 const comment_create= (req,res) =>{
     const newComment= new Comment(req.body);
     // console.log(newComment);
-    newComment.save().then((result) =>{
-        console.log(JSON.stringify(result));
-        res.send(res);
-    }).catch((err) =>{
-        console.log(err);
+    newComment.save().then((result) =>{ 
+        return res
+                .send({ message: "Comment sent",data:result});
+    }).catch((err) => {
+        return  res.send({ message: "Error sending comment",error: err.message })
     });
 
 }
+const comment_view= (req,res) => {
+  const id=req.params.id;
+       Comment.find({post_id:id,},{_id:0,__v:0}).then((result) => {
+             return res
+                .send({ message: "viewing comment",data:result});
+        }).catch((err) => {
+            return  res.send({ message: "Error viewing comment",error: err.message })
+        });
+}
+
+const comment_update = (req, res) => {
+    const id= req.params.id;
+    const UpdateComment= req.body;
+    Comment.findByIdAndUpdate(id, UpdateComment).then((result) => {
+         return res
+                .send({ message: "Comment updated",data:result});
+    }).catch((err) => {
+        return  res.send({ message: "Error updating comment",error: err.message })
+    });
+};
+
+
+const comment_delete =  async (req, res) => {
+    const id=req.params.id;
+      await Comment.findByIdAndDelete(id).then((result) => {
+             return res
+                    .send({ message: "Comment deleted"});
+        }).catch((err)=>{
+             return res
+                    .status(500)
+                    .json({ message: "Something went wrong", error: `Error: ${err}` });
+        });
+};
+
+
+
 
 
 
 module.exports= {
-  comment_create,comment_index
+  comment_create,comment_index,comment_view,comment_delete,comment_update
 }
--- routes/apis/comments.js
+++ routes/apis/comments.js
@@ -4,4 +4,7 @@ const commentController =  require('../../controllers/apis/commentsController');
 
 router.get('/all',commentController.comment_index)
 router.post('/create',commentController.comment_create);
+router.get('/post/:id',commentController.comment_view);
+router.put('/update/:id',commentController.comment_update);
+router.delete('/:id',commentController.comment_delete);
 module.exports =router;
