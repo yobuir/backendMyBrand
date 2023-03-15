c2719a886db9a65097e7116890645a18b2974188
jobeuse <iradukundajob3@gmail.com>
2023-03-15 18:31:33 +0200

    testing: all 25 test done with alsmost apis in test

--- routes/apis/comments.js
+++ routes/apis/comments.js
@@ -3,9 +3,9 @@ const router =express.Router();
 const commentController =  require('../../controllers/apis/commentsController');
 const {checkIfUserIsLogged,UserIsAdmin} = require('../../middleware/authMiddleware'); 
  
-router.get('/all',checkIfUserIsLogged,UserIsAdmin,commentController.comment_index)
-router.post('/create',checkIfUserIsLogged,commentController.comment_create);
+router.get('/all',commentController.comment_index);
 router.get('/post/:id',commentController.comment_view);
+router.post('/create',checkIfUserIsLogged,commentController.comment_create);
 router.put('/update/:id',checkIfUserIsLogged,commentController.comment_update);
 router.delete('/:id',checkIfUserIsLogged,commentController.comment_delete);
 module.exports =router;
